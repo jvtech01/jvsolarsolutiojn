fdec7bdc7196ddd87a715366be7776dfc607223e
Jules Jv <jvjules132@gmail.com>
2025-12-15 11:08:16 +0000

    MAKE IT DARK MODE DEFAULT

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
       <body className={`font-sans ${inter.variable} antialiased bg-background text-foreground`}>
         <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="dark"
             enableSystem
             disableTransitionOnChange
           >
